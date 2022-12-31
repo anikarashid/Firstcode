fbcf0ee7bacb18d217c293e5b27bcfd4def43c0a
anikarashid <anikarashi07771@gmail.com>
2022-12-31 17:27:17 +0900

    Window Handles

---
+++ src/main/java/ActionClass.java
@@ -0,0 +1,40 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionClass {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        WebElement scrollone = driver.findElement(By.xpath("//button[@id='mousehover']"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", scrollone);
+        Thread.sleep(2000L);
+
+     //   WebElement sd = driver.findElement(By.xpath("//a[text()='Latest News']"));
+
+        Actions a = new Actions(driver);
+        a.moveToElement(scrollone).perform();
+        //relod
+        a.moveToElement(driver.findElement(By.xpath("//a[text()='Reload']"))).click().perform();
+        WebElement scrolltwo = driver.findElement(By.xpath("//button[@id='mousehover']"));
+        JavascriptExecutor ps = (JavascriptExecutor) driver;
+        ps.executeScript("arguments[0].scrollIntoView();", scrolltwo);
+        a.moveToElement(scrolltwo).perform();
+        Thread.sleep(2000l);
+        //Top
+        a.moveToElement(driver.findElement(By.xpath("//a[text()='Top']"))).click().perform();
+
+        Thread.sleep(2000l);
+        driver.quit();
+
+
+
+    }
+}
---
+++ src/main/java/WindowHandels.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class WindowHandels {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.findElement(By.id("openwindow")).click();
+        Set<String> window = driver.getWindowHandles();
+        Iterator<String> it = window.iterator();
+        String parent = it.next();
+        String child = it.next();
+        driver.switchTo().window(parent);
+        String title = driver.getTitle();
+        System.out.println(title);
+        driver.switchTo().window(child);
+        String title1 = driver.getTitle();
+        System.out.println(title1);
+        driver.switchTo().window(parent);
+        String a= driver.getTitle();
+        System.out.println(a);
+        Thread.sleep(2000l);
+         driver.switchTo().window(child);
+         String x=driver.getTitle();
+         System.out.println(x);
+
+        Thread.sleep(3000l);
+        driver.quit();
+         driver.close();
+
+
+
+    }
+}

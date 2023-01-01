7e4de2dd4fca3cd9c9bd63b218957b7a29d73e0d
anikarashid <anikarashi07771@gmail.com>
2023-01-01 13:35:15 +0900

    Window Handles

--- src/main/java/WindowHandels.java
+++ src/main/java/WindowHandels.java
@@ -13,21 +13,29 @@ public class WindowHandels {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
         driver.findElement(By.id("openwindow")).click();
         Set<String> window = driver.getWindowHandles();
+
         Iterator<String> it = window.iterator();
+
         String parent = it.next();
         String child = it.next();
+
         driver.switchTo().window(parent);
         String title = driver.getTitle();
         System.out.println(title);
+
         driver.switchTo().window(child);
         String title1 = driver.getTitle();
         System.out.println(title1);
+
         driver.switchTo().window(parent);
         String a= driver.getTitle();
         System.out.println(a);
+
         Thread.sleep(2000l);
+
          driver.switchTo().window(child);
          String x=driver.getTitle();
          System.out.println(x);
